9436475036ca4af57944be0323495e288f40598d
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-05-07 19:13:16 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def Breed
+    @breed
+  end
+end
